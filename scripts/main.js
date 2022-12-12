19577d45d8295ddaabd4ff6fe5f3529f34ae6eac
araceliponce <113866541+araceliponce@users.noreply.github.com>
2022-12-12 11:33:57 -0500

    s

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,26 @@
-const daysInJanuary = getDaysInMonth(2025, 1);
-console.log(daysInJanuary); //>31
-
-let todayName = new Date().toLocaleDateString('es', { weekday: 'long' }); //>lunes   >newDate().getDay gives the number of day, like 3 for wednesday
-
-let todayNumber = new Date().getDate(); //numbers of day
-
+//GET CURRENT YEAR, MONTH
 const date = new Date();
 const currentYear = date.getFullYear();
 const currentMonth = date.getMonth() + 1; // since months are 0-based
-const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
-console.log(daysInCurrentMonth);
+
+// GET CURRENT DAYNAME, DAYNUMBER (LUNES, 12)
+let todayName = new Date().toLocaleDateString('es', { weekday: 'long' }); //>lunes   >newDate().getDay gives the number of day on week, like 3 for wednesday
+
+let todayNumber = new Date().getDate(); //number of day in month, today is 12
 
 
-//FUNCTIONS
+//GET HOW MANY DAYS IN MONTH
 function getDaysInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
+const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
+console.log(daysInCurrentMonth);
 
+const daysInJanuary = getDaysInMonth(2025, 1);
+console.log(daysInJanuary); //>31
+
+
+//GET THE FIRST DAY OF MONTHS
 function getFirstDayOfMonth(year, month) {
   return new Date(year, month, 1);
 }
@@ -26,14 +30,13 @@ const firstDayCurrentMonth = getFirstDayOfMonth(
   date.getMonth(),
 );
 console.log(firstDayCurrentMonth); //> Thu Dec 01 2022
-
 firstDayCurrentMonth.getDay(); //4 (jueves)
 
 const firstDayJanuary = getFirstDayOfMonth(2023, 0);
 console.log(firstDayJanuary); // Wed Jan 01 2025
 
 
-//LOOPS
+//CREATE BTNS AND PLACE THE FIRST ONE ON THEIR COLUMN
 //35 instead of daysInCurrentMonth
 for (i = 0; i < daysInCurrentMonth; i++) {
   let btnDia = document.createElement('button');
@@ -50,5 +53,3 @@ for (i = 0; i < daysInCurrentMonth; i++) {
 let firstDayBtn = document.querySelector('.calendar__dates button:first-of-type');
 console.log(firstDayBtn)
 firstDayBtn.style.gridColumn = firstDayCurrentMonth.getDay();
-
-
